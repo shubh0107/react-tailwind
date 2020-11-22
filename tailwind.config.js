4d14dc20d7c7c881a853c37a8628dcc409e4ac47
Shubham Singh <shubhsingh01@gmail.com>
2020-11-23 02:29:30 +0530

    Swapped the old tailwind config for a new one | Added a retro-shoe card component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
-// tailwind.config.js
 module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
   theme: {
     extend: {
+      boxShadow: {
+        'offset-black-sm': '2px 2px 0px 0px black',
+        'offset-black': '5px 5px 0px 0px black',
+        'offset-red': '5px 5px 0px 0px #fc8181'
+      },
       colors: {
         'clubhouse-gray': '#1d1f38',
-      }
+      },
+      spacing: {
+        36: '9rem',
+        52: '13rem',
+        76: '19rem'
+      },
+    },
+  },
+  variants: {
+    extend: {
+      // boxShadow: ['hover']
     }
-  }
+  },
+  plugins: [],
 }
