d95a983e1394c71bee4d78de4c0555429271eac9
Shubham Singh <shubhsingh01@gmail.com>
2021-03-15 01:01:16 +0530

    Fixed links transition in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,40 +5,32 @@ import { AnimateSharedLayout, motion, useMotionValue } from 'framer-motion'
 const Header = ({ routesList }) => {
   const { pathname } = useLocation();
 
-
   return <div className="h-16 px-8 flex border-b border-gray-400">
-    <AnimateSharedLayout>
-      <ul className="flex flex-none items-center space-x-2 text-indigo-500 ">
+    <ul className="flex flex-none items-center space-x-2 text-indigo-500">
+      <AnimateSharedLayout>
         {routesList.map(item => {
           return <HeaderLink data={item} key={item.path} activeLink={pathname} />
         })}
-      </ul>
-    </AnimateSharedLayout>
+      </AnimateSharedLayout>
+    </ul>
   </div >
 }
 
 const HeaderLink = ({ data, activeLink, ...restProps }) => {
   return <Link to={data.path} {...restProps}>
     {/* <span className={`${activeLink === data.path ? 'bg-indigo-500 text-white' : 'hover:bg-indigo-100'} p-2 rounded leading-none`}>{data.name}</span> */}
-    <li
-      // animate
-      className="text-indigo-500"
-      key={data.path}
-    // style={{ color: activeLink === data.path ? 'white' : "#667eea" }}
-
-    >
+    <li key={data.path}>
       {activeLink === data.path ?
         <motion.div
           layoutId="currentRoute"
           className="p-2 rounded leading-none bg-indigo-500 text-white"
-        // animate={{ background: '' }}
         >
           {data.name}
         </motion.div>
         :
-        <motion.div animate style={{ color: activeLink === data.path ? 'white' : "#667eea" }}>
+        <div className="p-2 rounded leading-none text-indigo-500 bg-white">
           {data.name}
-        </motion.div>
+        </div>
       }
     </li>
 
